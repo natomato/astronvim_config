4a96386c6a5a184138521cd14859eeb202621fef
Nater <natomato@users.noreply.github.com>
2024-10-15 13:57:51 -0400

    Add codecompanion to use anthropics ai for suggestions

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,32 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
+    "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
+    { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+  },
+  opts = {
+    strategies = {
+      chat = {
+        adapter = "anthropic",
+      },
+      inline = {
+        adapter = "anthropic",
+      },
+      agent = {
+        adapter = "anthropic",
+      },
+    },
+    adapters = {
+      anthropic = function()
+        return require("codecompanion.adapters").extend("anthropic", {
+          env = {
+            api_key = "cmd:op read op://private/anthropic/nvim-key --no-newline"
+          },
+        })
+      end,
+    },
+  }
+}
