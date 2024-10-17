473e86c5de5a7a6eea9def004cd8e6469c89f1d7
Nater <natomato@users.noreply.github.com>
2024-10-17 17:35:53 -0400

    feat(notifications): move notices to bottom right

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -22,4 +22,13 @@ return {
       },
     },
   },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 3000,
+      render = "compact",
+      stages = "fade",
+      top_down = false,
+    },
+  },
 }
